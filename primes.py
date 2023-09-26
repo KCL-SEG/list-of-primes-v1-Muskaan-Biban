cf6b7f77672a943650f9d1411f494e3306ab3ba5
k22037403 <k22037403@kcl.ac.uk>
2023-09-26 20:09:49 +0100

    intial

--- primes.py
+++ primes.py
@@ -3,4 +3,14 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes >= 1:
+        list.append(2)
+        if number_of_primes >= 2:
+            list.append(3)
+    number = 4
+    while len(list) > number_of_primes:
+        list.append(number)
+        number =+ 1
     return list
+
+primes(10)
